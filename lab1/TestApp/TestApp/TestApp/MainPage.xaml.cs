30054d0c8888347a5ee8d4a52bbba322b56f5581
Vilandino <sebastian.jankowiak@gmail.com>
2020-10-11 11:38:49 +0200

    Aplikacja_windows_android

---
+++ lab1/TestApp/TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Refresh(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(text.Text))
+            {
+                lbl.Text = "Witaj " + text.Text;
+                text.Text = "";
+            }
+            else
+            {
+                DisplayAlert("Błąd", "Pole jest puste", "Ok");
+                lbl.Text = "Uzupełnij pole";
+            }
+        }
+    }
+}
